dcf4df189e0c7bc69f4cdd104e04dcb96ff539ba
deekep <MVSein@stud.kantiana.ru>
2022-03-30 18:00:20 +0200

    new lib and tests

--- lib/MathUtils.hpp
+++ lib/MathUtils.hpp
@@ -1,28 +1,228 @@
 #pragma once
 #include <iostream>
 
-class Matrix{
-    public:
-        Matrix(int n, int m);
-        Matrix(const Matrix& mat);
-        Matrix& operator=(const Matrix& mat);
-        Matrix operator+(const Matrix& mat);
-        Matrix operator+=(const Matrix& mat);
-        Matrix operator*(const Matrix& mat);
-        Matrix operator-(const Matrix& mat);
-        Matrix operator-=(const Matrix& mat);
-        Matrix transposition();
-        int determinant();
-        Matrix inverse();
-        ~Matrix();
+namespace my::math{
+    //#define MY_DEBUG
 
-    friend std::istream& operator>>(std::istream& os, Matrix& mat);
-    friend std::ostream& operator<<(std::ostream& os, const Matrix& mat);
+    template<typename T, int N, int M>
+    struct MasWrapper{
+        T mas[N][M];
+    };
+
+    template<typename T, int N, int M>
+    class Matrix{
+    public:
+        Matrix(){
+        #ifdef MY_DEBUG
+            std::cout << "Constructor" << std::endl;
+        #endif
+            m_n = N;
+            m_m = M;
+            for (int i = 0; i < m_n; i++)
+                for (int j = 0; j < m_m; j++)
+                    m_mat[i][j] = 0;
+        }
+
+        Matrix(const T mas[N][M]){
+        #ifdef MY_DEBUG
+            std::cout << "Constructor" << std::endl;
+        #endif
+            m_n = N;
+            m_m = M;
+            for (int i = 0; i < m_n; i++)
+                for (int j = 0; j < m_m; j++)
+                    m_mat[i][j] = mas[i][j];
+        }
+
+        Matrix(const MasWrapper<T, N, M>& mas){
+        #ifdef MY_DEBUG
+            std::cout << "Constructor" << std::endl;
+        #endif
+            m_n = N;
+            m_m = M;
+            for (int i = 0; i < m_n; i++)
+                for (int j = 0; j < m_m; j++)
+                    m_mat[i][j] = mas.mas[i][j];
+        }
+
+        Matrix(const Matrix<T, N, M>& mat){
+        #ifdef MY_DEBUG
+            std::cout << "Copy constructor" << std::endl;
+        #endif
+
+            m_n = mat.m_n;
+            m_m = mat.m_m;
+
+            for (int i = 0; i < m_n; i++)
+                for (int j = 0; j < m_m; j++)
+                    m_mat[i][j] = mat.m_mat[i][j];
+        }
+
+        int getN() const { return m_n; }
+        int getM() const { return m_m; }
+        int get(int i, int j) const { return m_mat[i][j]; }
+        void set(int i, int j, T data) { m_mat[i][j] = data; }
+
+        template<typename A, int B, int C>
+        Matrix<T, N, M>& operator=(const Matrix<A, B, C>& mat){
+        #ifdef MY_DEBUG
+            std::cout << "Operator =" << std::endl;
+        #endif
+
+            m_n = mat.getN();
+            m_m = mat.getM();
+
+            for (int i = 0; i < m_n; i++)
+                for (int j = 0; j < m_m; j++)
+                    m_mat[i][j] = mat.get(i, j);
+
+            return *this;
+        }
+
+        template<typename A, int B, int C>
+        Matrix<T, N, M> operator+(const Matrix<A, B, C>& mat){
+        #ifdef MY_DEBUG
+            std::cout << "operator+" << std::endl;
+        #endif
+            Matrix<T, N, M> tmp;
+            for (int i = 0; i < m_n; i++)
+                for (int j = 0; j < m_m; j++)
+                    tmp.m_mat[i][j] = m_mat[i][j] + mat.m_mat[i][j];
+            return tmp;
+        }
+
+        template<typename A, int B, int C>
+        Matrix<T, N, M> operator*(const Matrix<A, B, C>& mat){
+        #ifdef MY_DEBUG
+            std::cout << "operator*" << std::endl;
+        #endif
+            Matrix<T, N, M> tmp;
+
+            for (int i = 0; i < m_n; i++)
+                for (int j = 0; j < mat.getM(); j++){
+                    int sum = 0;
+                    for (int k = 0; k < m_m; k++)
+                        sum += m_mat[i][k] * mat.get(i, j);
+                    tmp.set(i, j, sum);
+                }
+
+            return tmp;
+        }
+
+        template<typename A, int B, int C>
+        Matrix<T, N, M> operator-(const Matrix<A, B, C>& mat){
+        #ifdef MY_DEBUG
+            std::cout << "operator-" << std::endl;
+        #endif
+            Matrix<T, N, M> tmp;
+
+            for (int i = 0; i < m_n; i++)
+                for (int j = 0; j < m_m; j++)
+                    tmp.m_mat[i][j] = m_mat[i][j] - mat.m_mat[i][j];
+
+            return tmp;
+        }
+
+        Matrix transposition(){
+            Matrix<T, N, M> tmp;
+            for(int i = 0; i < m_n; i++)
+                for(int j = 0; j < m_m; j++)
+                    tmp.m_mat[i][j] = m_mat[j][i];
+            return tmp;
+        }
+
+        int determinant(){
+            int det = 0;
+            if(m_n == 2 && m_m == 2){
+                det = m_mat[0][0] * m_mat [1][1] - m_mat[0][1]*m_mat[1][0];
+                return det;
+            } else if(m_n == 3 && m_m == 3){
+                det = m_mat[0][0]*(m_mat[1][1]*m_mat[2][2] - m_mat[1][2]*m_mat[2][1]) -
+                      m_mat[0][1]*(m_mat[1][0]*m_mat[2][2] - m_mat[1][2]*m_mat[2][0]) +
+                      m_mat[0][2]*(m_mat[1][0]*m_mat[2][1] - m_mat[1][1]*m_mat[2][0]);
+                return det;
+            } else {
+                std::cout << "Вычисление определителя такого порядка не поддерживается" << std::endl;
+                return -1;
+            }
+        }
+
+        Matrix inverse(){
+            Matrix<T, N, M> tmp;
+            for(int i = 0; i < m_n; i++)
+                for(int j = 0; j < m_m; j++)
+                    tmp.m_mat[i][j] = 0;
+            if((m_n == 2 && m_m == 2) || (m_n == 3 && m_m == 3)){
+                int det = determinant();
+                if(det == 0) {
+                    std::cout << "Обратной матрицы не существует, так как определитель равен 0" << std::endl;
+                    return tmp;
+                }
+                if(m_n == 2){
+                    tmp.m_mat[0][0] = m_mat[1][1]/det;
+                    tmp.m_mat[0][1] = -m_mat[0][1]/det;
+                    tmp.m_mat[1][0] = -m_mat[1][0]/det;
+                    tmp.m_mat[1][1] = m_mat[0][0]/det;
+                }
+                if(m_n == 3){
+                    tmp.m_mat[0][0] = (m_mat[1][1]*m_mat[2][2] - m_mat[2][1]*m_mat[1][2])/det;
+                    tmp.m_mat[0][1] = -(m_mat[0][1]*m_mat[2][2] - m_mat[2][1]*m_mat[0][2])/det;
+                    tmp.m_mat[0][2] = (m_mat[0][1]*m_mat[1][2] - m_mat[1][1]*m_mat[0][2])/det;
+                    tmp.m_mat[1][0] = -(m_mat[1][0]*m_mat[2][2] - m_mat[2][0]*m_mat[1][2])/det;
+                    tmp.m_mat[1][1] = (m_mat[0][0]*m_mat[2][2] - m_mat[2][0]*m_mat[0][2])/det;
+                    tmp.m_mat[1][2] = -(m_mat[0][0]*m_mat[1][2] - m_mat[1][0]*m_mat[0][2])/det;
+                    tmp.m_mat[2][0] = (m_mat[1][0]*m_mat[2][1] - m_mat[2][0]*m_mat[1][1])/det;
+                    tmp.m_mat[2][1] = -(m_mat[0][0]*m_mat[2][1] - m_mat[2][0]*m_mat[0][1])/det;
+                    tmp.m_mat[2][2] = (m_mat[0][0]*m_mat[1][1] - m_mat[1][0]*m_mat[0][1])/det;
+                }
+            } else {
+                std::cout << "Вычисление обратной матрицы такого порядка не поддерживается" << std::endl;
+            }
+            return tmp;
+        }
+
+        ~Matrix(){
+        #ifdef MY_DEBUG
+            std::cout << "Destructor" << std::endl;
+        #endif
+        }
+
+        template<typename A, int B, int C>
+        friend std::istream& operator>>(std::istream& os, Matrix<A, B, C>& mat);
+
+        template<typename A, int B, int C>
+        friend std::ostream& operator<<(std::ostream& os, const Matrix<A, B, C>& mat);
 
     private:
         int m_n, m_m;
-        double** m_mat;
-};
+        T m_mat[N][M];
+    };
+
+    template<typename T, int N, int M>
+    std::istream& operator>>(std::istream& in, Matrix<T, N, M>& mat){
+        for (int i = 0; i < mat.m_n; i++)
+            for (int j = 0; j < mat.m_m; j++)
+                in >> mat.m_mat[i][j];
+        return in;
+    }
+
+    template<typename T, int N, int M>
+    std::ostream& operator<<(std::ostream& out, const Matrix<T, N, M>& mat){
+        out << "Matrix " << mat.m_n << "x" << mat.m_m << std::endl;
+        for (int i = 0; i < mat.m_n; i++) {
+            for (int j = 0; j < mat.m_m; j++)
+                out << mat.m_mat[i][j] << " ";
+            out << std::endl;
+        }
+        return out;
+    }
+
+    using Vec2i = Matrix<int, 2, 1>;
+    using Vec2d = Matrix<double, 2, 1>;
+    using Mat22i = Matrix<int, 2, 2>;
+    using Mat22d = Matrix<double, 2, 2>;
+}
+
 
 
 
--- main.cpp
+++ main.cpp
@@ -1,22 +1,101 @@
 #include "lib/MathUtils.hpp"
+using my::math::Mat22d;
+using my::math::Vec2d;
 
 int main() {
-    Matrix A(3,3);
-    std::cin >> A;
+    std::cout << "=== Test 1 : Multiplication ===" << std::endl;
+    {
+        Mat22d A({ {
+                           {1,2},
+                           {3,4}
+                   } });
 
-    Matrix B(3,3);
-    std::cin >> B;
+        Vec2d X({ {
+                          {1},
+                          {1}
+                  } });
 
-    Matrix C(3,3);
+        auto B = A * X;
 
-    C = A*B;
-    std::cout << C << std::endl;
+        assert(B.get(0, 0) == 3);
+        assert(B.get(1, 0) == 7);
+    }
+    std::cout << "Done!" << std::endl;
 
-    C = A-B;
-    std::cout << C << std::endl;
+    std::cout << "=== Test 2 : Addition ===" << std::endl;
+    {
+        Mat22d A({ {
+                           {1,1},
+                           {1,1}
+                   } });
 
-    std::cout << A.inverse() << std::endl;
-    std::cout << A.determinant() << std::endl;
-    std::cout << A.transposition() << std::endl;
+        Mat22d X({ {
+                          {1},
+                          {1}
+                  } });
+
+        auto B = A + X;
+        assert(B.get(0, 0) == 2);
+        assert(B.get(1, 0) == 2);
+    }
+    std::cout << "Done!" << std::endl;
+
+    std::cout << "=== Test 3 : Subtraction ===" << std::endl;
+    {
+        Mat22d A({ {
+                           {1,1},
+                           {1,1}
+                   } });
+
+        Mat22d X({ {
+                           {1},
+                           {1}
+                   } });
+
+        auto B = A - X;
+        assert(B.get(0, 0) == 0);
+        assert(B.get(1, 0) == 0);
+    }
+    std::cout << "Done!" << std::endl;
+
+    std::cout << "=== Test 4 : Transposition ===" << std::endl;
+    {
+        Mat22d A({ {
+                           {1,2},
+                           {3,4}
+                   } });
+
+        auto B = A.transposition();
+        assert(B.get(0, 1) == 3);
+        assert(B.get(1, 0) == 2);
+    }
+    std::cout << "Done!" << std::endl;
+
+    std::cout << "=== Test 5 : Determinant ===" << std::endl;
+    {
+        Mat22d A({ {
+                           {1,2},
+                           {3,4}
+                   } });
+
+        auto B = A.determinant();
+        assert(B == -2);
+    }
+    std::cout << "Done!" << std::endl;
+
+    std::cout << "=== Test 6 : Inverse ===" << std::endl;
+    {
+        Mat22d A({ {
+                           {1,2},
+                           {3,4}
+                   } });
+
+        auto B = A.inverse();
+        assert(B.get(0,0) == -2);
+        assert(B.get(0,1) == 1);
+    }
+    std::cout << "Done!" << std::endl;
+
+
     return 0;
 }
